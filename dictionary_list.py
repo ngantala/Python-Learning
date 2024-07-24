29ec4ed8930e6366ccab73c23069192bcac75eb6
ngantala <131758237+ngantala@users.noreply.github.com>
2024-07-24 08:31:01 -0400

    Create dictionary_list.py

---
+++ dictionary_list.py
@@ -0,0 +1,13 @@
+route53_record = [{
+  "name": "x.example.com",
+  "type": "TXT",
+  "value": "yah00=12133ldkldjd"
+},
+
+                {
+  "name": "y.example.com",
+  "type": "TXT",
+  "value": "yahoo=23313213"
+                }
+                 ]
+
