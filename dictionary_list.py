ffc239b3e79b1003e3309842482f57e6518563d9
ngantala <131758237+ngantala@users.noreply.github.com>
2024-07-24 08:33:53 -0400

    Update dictionary_list.py

--- dictionary_list.py
+++ dictionary_list.py
@@ -1,13 +1,14 @@
-route53_record = [{
+route53_record = [
+  {
   "name": "x.example.com",
   "type": "TXT",
   "value": "yah00=12133ldkldjd"
-},
-
-                {
+  },
+  {
   "name": "y.example.com",
   "type": "TXT",
   "value": "yahoo=23313213"
-                }
-                 ]
+  }
+]
+print(route53_record[0]["value"])
 
