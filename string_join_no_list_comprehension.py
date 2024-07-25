697527abc0663b42b9bb0bd6d23e64ecaab84cea
ngantala <131758237+ngantala@users.noreply.github.com>
2024-07-25 05:26:40 -0400

    Update string_join_no_list_comprehension.py

--- string_join_no_list_comprehension.py
+++ string_join_no_list_comprehension.py
@@ -1,4 +1,10 @@
 record = "x.example.com"
 
 spliting = record.split(".")
-print(splitting)
+print(spliting)
+
+new_list = []
+
+for i in range(len(spliting)-1):
+    new_list.append(".".join(spliting[i:]))
+    print(new_list)
