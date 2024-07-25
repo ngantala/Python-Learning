a212a32c6d9456463184b7d3891131a1d0d015a7
ngantala <131758237+ngantala@users.noreply.github.com>
2024-07-25 04:40:12 -0400

    Update string_join.py

--- string_join.py
+++ string_join.py
@@ -1,13 +1,8 @@
 record = "x.example.com"
 splitting = record.split(".")
 print(splitting)
-
 rejoin = [".".join(splitting)]
 print(rejoin)
-
 print([".".join(splitting[i:]) for i in range(len(splitting))])
-
 print([".".join(splitting[i:]) for i in range(len(splitting)-1)])
-
-
 print([i for i in range(5)])
